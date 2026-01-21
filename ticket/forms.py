79e859147220b528947f4cc559d312d99c1017d1
Siddhant Mahajan <siddhant.mahajan@qure.ai>
2026-01-21 13:47:08 +0530

    removed ticket/forms.py

--- ticket/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Ticket
-
-class TicketForm(forms.ModelForm):
-    class Meta:
-        model = Ticket
-        fields = ['title', 'description'] # We exclude 'author' and 'status' so users can't fake them
