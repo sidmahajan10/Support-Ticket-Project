f5756b6b734b626fff24d7115d708372a262d714
Siddhant Mahajan <siddhant.mahajan@qure.ai>
2026-01-21 13:54:14 +0530

    removed user/forms.py

--- user/forms.py
+++ user/forms.py
@@ -1,9 +1,9 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from .models import User
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+# from .models import User
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = User
-        # Add 'name' to the fields list
-        fields = UserCreationForm.Meta.fields + ('name',)
+# class CustomUserCreationForm(UserCreationForm):
+#     class Meta(UserCreationForm.Meta):
+#         model = User
+#         # Add 'name' to the fields list
+#         fields = UserCreationForm.Meta.fields + ('name',)
